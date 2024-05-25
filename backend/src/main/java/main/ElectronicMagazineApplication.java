8c5973698cd7f75022e1ef30c681bc4ee00c2c37
klukv <tema200260@gmail.com>
2024-05-25 15:51:21 +0300

    backend: create authorization and registration  - controller  - dto  - service  - JPA repositories

---
+++ backend/src/main/java/main/ElectronicMagazineApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicMagazineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/java/main/electronic_magazine/ElectronicMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package java.main.electronic_magazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
